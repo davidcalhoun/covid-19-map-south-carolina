ce7f9d2b56831cc131629d282ec4735e45c8dc90
David Calhoun <david.b.calhoun@gmail.com>
2020-04-24 13:21:26 -0400

    update data for April 22

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, normalize } from 'path';
 
-const date = '2020-04-21';
+const date = '2020-04-22';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
--- src/consts.js
+++ src/consts.js
@@ -103,6 +103,11 @@ export const casesData = [
 		filename: "2020-04-21.json",
 		dayOfYear: 112,
 		sourceUrl: "https://github.com/davidcalhoun/covid-19-map-south-carolina/blob/master/src/data/2020-04-21.pdf"
+	},
+	{
+		filename: "2020-04-22.json",
+		dayOfYear: 113,
+		sourceUrl: "https://github.com/davidcalhoun/covid-19-map-south-carolina/blob/master/src/data/2020-04-22.pdf"
 	}
 ];
 
